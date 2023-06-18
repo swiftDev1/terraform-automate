0481c491ba8a8a3da245e7be00e25b30610581a1
Chika Hope <elumeziemchikahope@gmail.com>
2023-06-18 13:53:05 +0100

    added the s3 backend configuraation to the terraform block and re-initialized the project

--- provider.tf
+++ provider.tf
@@ -1,10 +1,19 @@
 terraform {
+
+  backend "s3" {
+    bucket         = "saucecode-terraform-remote-state-backend-bucket"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform_state"
+  }
+
   required_providers {
     aws = {
       source = "hashicorp/aws"
       version = "5.4.0"
     }
   }
+
 }
 
 provider "aws" {
